2002e85cffb74eafbd965d3eb2784d6c567ff5c0
leonardusluwianto <leonardusluwianto@gmail.com>
2021-04-29 00:21:51 +0700

    rapikan folder

--- database.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con = mysqli_connect('sql6.freesqldatabase.com','sql6408872','SxbKzJecZN','sql6408872');
-?>
---
+++ src/database.inc.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect('sql6.freesqldatabase.com','sql6408872','SxbKzJecZN','sql6408872');
+?>
